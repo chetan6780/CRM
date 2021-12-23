6884e64de1db12afd382b3cb02c1cc239cb42b1d
chetan6780 <crp20802@gmail.com>
2021-12-24 00:13:02 +0530

    Staticfiles

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -120,10 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'leads.User'
+AUTH_USER_MODEL = 'leads.User' # use/make User of leads app as auth user model
--- notes.md
+++ notes.md
@@ -60,3 +60,4 @@
 -   Extended Base.html
 -   Added Tailwind CSS
 -   Added & Extended Class base generic views to shorten the `views.py`
+-   Staticfiles configured
